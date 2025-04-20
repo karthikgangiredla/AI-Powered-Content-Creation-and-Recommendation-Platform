17bd75ea73cb0873d184cc5e4e24575569a6d723
Karthik <gangiredlakarthik14@gmail.com>
2025-04-20 22:46:42 +0530

    similarity testing

--- test_similarity.py
+++ test_similarity.py
@@ -1,8 +1,6 @@
 from embedder import get_similar_articles
-
 query = "Explain vector databases and how they work"
 results = get_similar_articles(query_text=query)
-
 print("Top Similar Articles:")
 for idx, meta in enumerate(results["metadatas"][0]):
     print(f"{idx + 1}. {meta['title']} (ID: {results['ids'][0][idx]})")
